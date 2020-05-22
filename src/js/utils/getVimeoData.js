ece02b2165dce6f565c90df61d834f08780badeb
Ömür Yanıkoğlu <hayatbiralem@gmail.com>
2020-05-22 03:31:46 +0300

    https issue on getting vimeo data fixed

--- src/js/utils/getVimeoData.js
+++ src/js/utils/getVimeoData.js
@@ -1,4 +1,4 @@
 import fetch from 'node-fetch';
 export default function getVimeoData(id) {
-  return fetch('http://vimeo.com/api/v2/video/'+id+'.json');
+  return fetch('//vimeo.com/api/v2/video/'+id+'.json');
 }
